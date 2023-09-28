97de8d1b0c500deaae104594c13cb922faace4b1
Max Sandoval <68491989+morpheomax@users.noreply.github.com>
2023-09-28 18:44:12 -0300

    Actualización Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="h-20  w-full bg-slate-200">
-      <div className="flex  items-center justify-between ">
-        <h2>Footer</h2>
-      </div>
+    <footer className="h-20 w-full bg-slate-200 flex items-center justify-center">
+      <h2 className="text-center">Website developed by Max Sandoval</h2>
     </footer>
   );
 };
--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -53,7 +53,7 @@ const FormContact = () => {
   return (
     <>
       <div className="max-w-3xl mx-auto p-4 border rounded-md shadow-md">
-        <h2 className="text-2xl font-semibold mb-4">Contáctame</h2>
+        <h2 className="text-2xl font-semibold mb-4 text-center">Contáctame</h2>
         <form
           onSubmit={guardarContact}
           className="grid grid-cols-1 md:grid-cols-2 gap-4"
